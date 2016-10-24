61107cbad97852b78124e4b26efb98f5a4e96004
Xaree Lee <xareelee@gmail.com>
2016-10-24 16:42:41 +0800

    Setup a Xcode project for build

---
+++ XLReactiveCocoaExtensionsDev/XLReactiveCocoaExtensionsDev/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLReactiveCocoaExtensionsDev
+//
+//  Created by Xaree on 10/24/16.
+//  Copyright © 2016 Xaree Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
